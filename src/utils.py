693898bc39e1e2f6f651c32096e9f5b66539d6b5
Honey <honeygupta1265@gmail.com>
2025-04-04 22:16:55 +0530

    Prediction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ from src.exception import CustomException
 import sys
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
+
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
@@ -31,13 +32,19 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
             model.fit(X_train,y_train)
 
             y_test_pred = model.predict(X_test)
-
-
             test_model_score = r2_score(y_test, y_test_pred)
 
             report[list(models.keys())[i]] = test_model_score
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
